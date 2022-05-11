46aa7b3cd5ac3f1a348e5c98e91064c413e10d86
enesergun1515@gmail.com <63712936+enesergun@users.noreply.github.com>
2022-05-11 20:19:39 +0300

    folder arch make

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import {useState, useEffect} from 'react'
 
 import './App.css';
 
-import image1 from './assests/image1.png'
-import image2 from './assests/image2.png'
-
 import axios from 'axios'
+import Pagination from './components/Pagination';
+import Header from './components/Header'
+import CharactersCard from './components/CharactersCard'
 
 // https://gateway.marvel.com/v1/public/characters?ts=1&apikey=89c5bb6f000ff89c6b3bfd1804a55184&hash=d8e15a485cc807f99e27672c604d81c5&limit=100&offset=0
 
@@ -14,63 +14,28 @@ function App() {
   const [list, setList] = useState([]);
   const [offset, setOffset] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
-  const [pageCount, setPageCount] = useState()
+  const [pageCount, setPageCount] = useState(78)
+  const [total] = useState(78)
 
   
   useEffect(() => {
     getData(currentPage);
   }, [currentPage])
 
-  
-/* HANDLE FUNCTIONS */  
-const handlePrevButton = () => {
-  if (offset >= 20){
+
+const setPage = (value) => {
+  if (value.type === "next" && currentPage < total) {
+    setCurrentPage((x) => x + 1);
+    setOffset(offset + 20);
+  } else if (value.type === "prev" && currentPage > 0) {
+    setCurrentPage((x) => x - 1);
     setOffset(offset - 20);
-    setCurrentPage(currentPage - 1); 
+  } else if (value.type === "add") {
+    setOffset((currentPage - 1) * 20);
+    setCurrentPage(value.number);
     
   }
-}
-
-const handleNextButton = () => {
-  setOffset(offset + 20);
-  setCurrentPage(currentPage + 1);
-  
-}
-
-const handleChangePage = (page) => {
-  setOffset((page - 1) * 20);
-  setCurrentPage(page);
-  console.log("hangleChangePage", page);
-}
-
-/* THIS FUNC. IS CREAT PAGE NUMBERS IN PAGINATION */
-const getPaginationGroup = () => {
-  let start = ((currentPage - 1) / 5) * 5;
-
-  let arr = new Array(3).fill().map((_, index) => {
-    if (start === 78) {
-      return "" 
-    } 
-    return start + index + 1
-  });
-
-  
-  if (start >= 4 && start < 73) {
-    return [1, "...", start - 1, start, ...arr, "...", pageCount]
-
-  } else if (start < 4) {
-    return [1, 2, 3, 4, 5, "...", 78]
-
-  } else if (start >= 73 && start <= 78) {
-    return [1, "...", pageCount- 5, pageCount- 4, pageCount-3, pageCount-2, pageCount-1, pageCount]
-
-  } else {
-    return [...arr, "...", pageCount]
-
-  }
 };
-
-
 
 // request to marvel.api
   const getData = (currentPage) => {
@@ -99,36 +64,22 @@ const getPaginationGroup = () => {
   });
   }
 }
-
+console.log(list);
 
   return (
-    /* HEADER START */
+    
     <div className="main">
-        <div className="header">
-            <div className="header-background">
-                <img className="header-background-img" src={image1} alt="" />                
-            </div>
-            <div className="header-logo">
-                <img className="header-logo-img" src={image2} alt="" />
-            </div>
-        </div>
-        {/* HEADER END */}
+        <Header />
+        
 
         {/* CONTENT START */}
+        
         <div className="container">
           {            
             list.length > 0 &&
-            list?.map((item, index) => (
+            list.map((item, index) => (
+              <CharactersCard item={item} key={index} />
               
-              <div key={index} className="character-rectangle">
-                <div className="top-line"></div>
-                <div className="character-image-wrap">
-                  <img className='character-image' src={`${item.thumbnail.path}/portrait_xlarge.${item.thumbnail.extension}`} alt="" />
-                </div>
-                <div className="character-name">
-                  <p>{item.name}</p>
-              </div>
-              </div>
             )) 
           }
 
@@ -136,36 +87,7 @@ const getPaginationGroup = () => {
           {/* CONTENT END */}
 
         {/* PAGINATION START */}
-        <div className="pagination">
-            {
-              currentPage === 1 
-              ? <div></div> 
-              : (
-              <div onClick={handlePrevButton} className="previous-page">
-                {"<"}
-              </div>
-            )
-            }
-            <div className="pages">
-              { 
-                getPaginationGroup().map((page, index) => (
-                  <div key={index} className="page-number">
-                    <div onClick={(e) => {handleChangePage(e.target.textContent) }} className={`${currentPage  == page ? 'active-page' : null}`}>{page}</div>
-                  </div>
-                ))                
-              }                            
-            </div>            
-            {
-              currentPage === 78 
-              ? <div></div>
-              : 
-              (
-                <div onClick={handleNextButton} className="next-page">
-                  {">"}
-                </div>
-              )
-            }
-        </div>
+        <Pagination total={total} currentPage={currentPage} click={setPage}/>        
         {/* PAGINATION END */}
     </div>
   );
