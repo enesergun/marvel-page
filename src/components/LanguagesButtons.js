0c6283bfad713f381e12ad89afa4bf7a87797a8d
enesergun1515@gmail.com <63712936+enesergun@users.noreply.github.com>
2022-05-11 21:59:49 +0300

    active button style

--- src/components/LanguagesButtons.js
+++ src/components/LanguagesButtons.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import i18n from '../i18next';
 
+
 const LanguagesButtons = () => {
+
+  const [activeLang, setActiveLang] = useState('tr');
+
+  const changeLang = lang => {
+    i18n.changeLanguage(lang);
+    setActiveLang(lang);
+  }
+
   return (
     <div className='LangButtons'>
-      <button className='langButton'>FR</button>
-      <button className='langButton'>EN</button>
-      <button className='langButton'>TR</button>
+      <button className={activeLang === 'fr' ? 'langButton langButtonsActive' : 'langButton'} onClick={() => changeLang('fr')}>FR</button>
+      <button className={activeLang === 'en' ? 'langButton langButtonsActive' : 'langButton'} onClick={() => changeLang('en')}>EN</button>
+      <button className={activeLang === 'tr' ? 'langButton langButtonsActive' : 'langButton'} onClick={() => changeLang('tr')}>TR</button>
     </div>
   )
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+
+import { useTranslation } from 'react-i18next';
 
 const SearchBar = () => {
+
+    const { t } = useTranslation();
+
   return (
     <div className="searchBar">
-        <label htmlFor="">Karakter Ara</label>
-        <input type="search" name="characterSearch" id="" placeholder='' />
+        <label htmlFor="">{t('searchCharacter')}</label>
+        <input type="search" name="characterSearch" id="" placeholder={t('searchCharacterName')} />
     </div>
   )
 }
