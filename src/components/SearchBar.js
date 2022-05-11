05b534201dbabaaec1b2877e0b4a1708f1fa0b79
enesergun1515@gmail.com <63712936+enesergun@users.noreply.github.com>
2022-05-11 23:18:15 +0300

    will complete  search onChange function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,13 +3,17 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 const SearchBar = () => {
-
+/* https://gateway.marvel.com/v1/public/characters?ts=1&apikey=89c5bb6f000ff89c6b3bfd1804a55184&hash=d8e15a485cc807f99e27672c604d81c5&nameStartsWith= */
     const { t } = useTranslation();
+
+    const handleChar = (charName) => {
+        console.log(charName.target.value);
+    }
 
   return (
     <div className="searchBar">
         <label htmlFor="">{t('searchCharacter')}</label>
-        <input type="search" name="characterSearch" id="" placeholder={t('searchCharacterName')} />
+        <input type="text" name="characterSearch" id="" placeholder={t('searchCharacterName')} onChange={handleChar}/>
     </div>
   )
 }
